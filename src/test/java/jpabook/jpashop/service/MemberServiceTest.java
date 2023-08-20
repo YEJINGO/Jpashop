b6d5baaac9070fc4bf364e5a0c8a7570a0941beb
Yejin <yaejingo@gmail.com>
2023-08-20 23:04:54 +0900

    Test Code 주석

--- src/test/java/jpabook/jpashop/service/MemberServiceTest.java
+++ src/test/java/jpabook/jpashop/service/MemberServiceTest.java
@@ -1,50 +1,50 @@
-package jpabook.jpashop.service;
-
-import jpabook.jpashop.domain.Member;
-import jpabook.jpashop.repository.MemberRepository;
-import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.transaction.annotation.Transactional;
-
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.fail;
-
-@SpringBootTest
-@Transactional
-class MemberServiceTest {
-    @Autowired
-    MemberService memberService;
-    @Autowired
-    MemberRepository memberRepository;
-
-    @Test
-    @DisplayName("회원가입")
-    public void join() throws Exception {
-        Member member = new Member();
-        member.setName("Go");
-
-        Long saveId = memberService.join(member);
-
-//        Assertions.assertThat(memberRepository.findOne(saveId)).isEqualTo(member.getId());
-        assertEquals(member, memberRepository.findOne(saveId));
-    }
-
-    @Test
-    @DisplayName("중복 회원 예외")
-    public void exception() throws Exception {
-        //given
-        Member member1 = new Member();
-        member1.setName("Go");
-
-        Member member2 = new Member();
-        member2.setName("Go");
-        //when
-        memberService.join(member1);
-        memberService.join(member2);
-        //then
-        fail("예외 발생 ");
-    }
-}
+//package jpabook.jpashop.service;
+//
+//import jpabook.jpashop.domain.Member;
+//import jpabook.jpashop.repository.MemberRepository;
+//import org.assertj.core.api.Assertions;
+//import org.junit.jupiter.api.DisplayName;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.transaction.annotation.Transactional;
+//
+//import static org.junit.Assert.assertEquals;
+//import static org.junit.Assert.fail;
+//
+//@SpringBootTest
+//@Transactional
+//class MemberServiceTest {
+//    @Autowired
+//    MemberService memberService;
+//    @Autowired
+//    MemberRepository memberRepository;
+//
+//    @Test
+//    @DisplayName("회원가입")
+//    public void join() throws Exception {
+//        Member member = new Member();
+//        member.setName("Go");
+//
+//        Long saveId = memberService.join(member);
+//
+////        Assertions.assertThat(memberRepository.findOne(saveId)).isEqualTo(member.getId());
+//        assertEquals(member, memberRepository.findOne(saveId));
+//    }
+//
+//    @Test
+//    @DisplayName("중복 회원 예외")
+//    public void exception() throws Exception {
+//        //given
+//        Member member1 = new Member();
+//        member1.setName("Go");
+//
+//        Member member2 = new Member();
+//        member2.setName("Go");
+//        //when
+//        memberService.join(member1);
+//        memberService.join(member2);
+//        //then
+//        fail("예외 발생 ");
+//    }
+//}
--- src/test/java/jpabook/jpashop/service/OrderServiceTest.java
+++ src/test/java/jpabook/jpashop/service/OrderServiceTest.java
@@ -1,69 +1,69 @@
-package jpabook.jpashop.service;
-
-import jpabook.jpashop.domain.Address;
-import jpabook.jpashop.domain.Member;
-import jpabook.jpashop.domain.Order;
-import jpabook.jpashop.domain.OrderStatus;
-import jpabook.jpashop.domain.item.Book;
-import jpabook.jpashop.domain.item.Item;
-import jpabook.jpashop.repository.OrderRepository;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.transaction.annotation.Transactional;
-
-import static org.junit.Assert.assertEquals;
-
-
-import javax.persistence.EntityManager;
-
-@SpringBootTest
-@Transactional
-class OrderServiceTest {
-    @Autowired
-    EntityManager em;
-    @Autowired
-    OrderService orderService;
-    @Autowired
-    OrderRepository orderRepository;
-
-    @Test
-    @DisplayName("상품 주문")
-    public void orderItem() throws Exception {
-        //given
-        Member member = createMember();
-        Item item = createBook("JPA", 32000, 10);
-        int orderCount = 2;
-
-        //when
-        Long orderId = orderService.order(member.getId(), item.getId(), orderCount);
-
-        //then
-        Order getOrder = orderRepository.findOne(orderId);
-
-        assertEquals("상품주문시 상태는 ORDER", OrderStatus.ORDER, getOrder.getStatus());
-        assertEquals("주문한 상품 종류 수가 정확해야 한다.", 1, getOrder.getOrderItems().size());
-        assertEquals("주문 가격은 가격 * 수량이다.", 32000 * 2, getOrder.getTotalPrice());
-        assertEquals("주문 수량만큼 재고가 줄어야 한다.", 8, item.getStockQuantity());
-
-    }
-
-    private Item createBook(String name, int price, int stockQuantity) {
-        Book book = new Book();
-        book.setName(name);
-        book.setPrice(price);
-        book.setStockQuantity(stockQuantity);
-        em.persist(book);
-        return book;
-    }
-
-    private Member createMember() {
-        Member member = new Member();
-        member.setName("회원1");
-        member.setAddress(new Address("서울", "거리1", "123"));
-        em.persist(member);
-        return member;
-    }
-}
+//package jpabook.jpashop.service;
+//
+//import jpabook.jpashop.domain.Address;
+//import jpabook.jpashop.domain.Member;
+//import jpabook.jpashop.domain.Order;
+//import jpabook.jpashop.domain.OrderStatus;
+//import jpabook.jpashop.domain.item.Book;
+//import jpabook.jpashop.domain.item.Item;
+//import jpabook.jpashop.repository.OrderRepository;
+//import org.junit.jupiter.api.Assertions;
+//import org.junit.jupiter.api.DisplayName;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.transaction.annotation.Transactional;
+//
+//import static org.junit.Assert.assertEquals;
+//
+//
+//import javax.persistence.EntityManager;
+//
+//@SpringBootTest
+//@Transactional
+//class OrderServiceTest {
+//    @Autowired
+//    EntityManager em;
+//    @Autowired
+//    OrderService orderService;
+//    @Autowired
+//    OrderRepository orderRepository;
+//
+//    @Test
+//    @DisplayName("상품 주문")
+//    public void orderItem() throws Exception {
+//        //given
+//        Member member = createMember();
+//        Item item = createBook("JPA", 32000, 10);
+//        int orderCount = 2;
+//
+//        //when
+//        Long orderId = orderService.order(member.getId(), item.getId(), orderCount);
+//
+//        //then
+//        Order getOrder = orderRepository.findOne(orderId);
+//
+//        assertEquals("상품주문시 상태는 ORDER", OrderStatus.ORDER, getOrder.getStatus());
+//        assertEquals("주문한 상품 종류 수가 정확해야 한다.", 1, getOrder.getOrderItems().size());
+//        assertEquals("주문 가격은 가격 * 수량이다.", 32000 * 2, getOrder.getTotalPrice());
+//        assertEquals("주문 수량만큼 재고가 줄어야 한다.", 8, item.getStockQuantity());
+//
+//    }
+//
+//    private Item createBook(String name, int price, int stockQuantity) {
+//        Book book = new Book();
+//        book.setName(name);
+//        book.setPrice(price);
+//        book.setStockQuantity(stockQuantity);
+//        em.persist(book);
+//        return book;
+//    }
+//
+//    private Member createMember() {
+//        Member member = new Member();
+//        member.setName("회원1");
+//        member.setAddress(new Address("서울", "거리1", "123"));
+//        em.persist(member);
+//        return member;
+//    }
+//}
